7ec23ae0d93e2ded90b3b895fb45cbe5dee52098
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-04-28 20:19:24 -0700

    Tweaked task behavior

--- katbench.py
+++ katbench.py
@@ -191,9 +191,7 @@ for key, value in language_datasets.items():
 TASKS_TABLE = language_tasks
 
 import os
-if os.path.exists("katbench.txt"):
-    os.remove("katbench.txt")
-
-with open("katbench.txt", "w") as f:
-    for task in TASKS_TABLE:
-        f.write(task.suite[0] + "|" + task.name + "|0|0\n")
+if not os.path.exists("katbench.txt"):
+    with open("katbench.txt", "w") as f:
+        for task in TASKS_TABLE:
+            f.write(task.suite[0] + "|" + task.name + "|0|0\n")
