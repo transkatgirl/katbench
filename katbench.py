6bc1426361c91f103c886a2249021bd37baaaef6
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-04-29 16:01:15 -0700

    Tweaked active tasks

--- katbench.py
+++ katbench.py
@@ -15,131 +15,131 @@ language_datasets = {
         "hf_subsets": ["sample-10BT"],
         "evaluation_splits": ["train"],
     },
-    "twitteraae": {
-        "hf_repo": "lighteval/TwitterAAE",
-        "hf_subsets": ["aa", "white"],
-        "evaluation_splits": ["test"],
-    },
-    "culturax:eng": {
-        "hf_repo": "yiyic/culturaX_eng",
-        "hf_subsets": [],
-        "evaluation_splits": ["test"],
-    },
-    "culturax:deu": {
-        "hf_repo": "yiyic/culturaX_deu",
-        "hf_subsets": [],
-        "evaluation_splits": ["test"],
-    },
-    "culturax:esp": {
-        "hf_repo": "yiyic/culturaX_esp",
-        "hf_subsets": [],
-        "evaluation_splits": ["test"],
-    },
-    "culturax:fra": {
-        "hf_repo": "yiyic/culturaX_fra",
-        "hf_subsets": [],
-        "evaluation_splits": ["test"],
-    },
-    "culturax:ja": {
-        "hf_repo": "yiyic/CulturaX_ja_test",
-        "hf_subsets": [],
-        "evaluation_splits": ["train"],
-    },
-    "culturax:yi": {
-        "hf_repo": "yiyic/CulturaX_yi_test",
-        "hf_subsets": [],
-        "evaluation_splits": ["train"],
-    },
-    "culturax:he": {
-        "hf_repo": "yiyic/CulturaX_he_test",
-        "hf_subsets": [],
-        "evaluation_splits": ["train"],
-    },
-    "culturax:am": {
-        "hf_repo": "yiyic/CulturaX_am_test",
-        "hf_subsets": [],
-        "evaluation_splits": ["train"],
-    },
-    "culturax:mt": {
-        "hf_repo": "yiyic/CulturaX_mt_test",
-        "hf_subsets": [],
-        "evaluation_splits": ["train"],
-    },
-    "culturax:hi": {
-        "hf_repo": "yiyic/CulturaX_hi_test",
-        "hf_subsets": [],
-        "evaluation_splits": ["train"],
-    },
-    "culturax:ur": {
-        "hf_repo": "yiyic/CulturaX_ur_test",
-        "hf_subsets": [],
-        "evaluation_splits": ["train"],
-    },
-    "culturax:gu": {
-        "hf_repo": "yiyic/CulturaX_gu_test",
-        "hf_subsets": [],
-        "evaluation_splits": ["train"],
-    },
-    "culturax:si": {
-        "hf_repo": "yiyic/CulturaX_si_test",
-        "hf_subsets": [],
-        "evaluation_splits": ["train"],
-    },
-    "culturax:pa": {
-        "hf_repo": "yiyic/CulturaX_pa_test",
-        "hf_subsets": [],
-        "evaluation_splits": ["train"],
-    },
-    "culturax:tr": {
-        "hf_repo": "yiyic/CulturaX_tr_test",
-        "hf_subsets": [],
-        "evaluation_splits": ["train"],
-    },
-    "culturax:kk": {
-        "hf_repo": "yiyic/CulturaX_kk_test",
-        "hf_subsets": [],
-        "evaluation_splits": ["train"],
-    },
-    "culturax:ko": {
-        "hf_repo": "yiyic/CulturaX_ko_test",
-        "hf_subsets": [],
-        "evaluation_splits": ["train"],
-    },
-    "culturax:mn": {
-        "hf_repo": "yiyic/CulturaX_mn_test",
-        "hf_subsets": [],
-        "evaluation_splits": ["train"],
-    },
-    "culturax:mhr": {
-        "hf_repo": "yiyic/CulturaX_mhr_test",
-        "hf_subsets": [],
-        "evaluation_splits": ["train"],
-    },
-    "culturax:fi": {
-        "hf_repo": "yiyic/CulturaX_fi_test",
-        "hf_subsets": [],
-        "evaluation_splits": ["train"],
-    },
-    "culturax:ar": {
-        "hf_repo": "yiyic/CulturaX_ar_test",
-        "hf_subsets": [],
-        "evaluation_splits": ["train"],
-    },
-    "culturax:hu": {
-        "hf_repo": "yiyic/CulturaX_hu_test",
-        "hf_subsets": [],
-        "evaluation_splits": ["train"],
-    },
-    "culturax:zh": {
-        "hf_repo": "yiyic/CulturaX_zh_test",
-        "hf_subsets": [],
-        "evaluation_splits": ["train"],
-    },
-    "the-stack": {
-        "hf_repo": "bigcode/the-stack-smol-xs",
-        "hf_subsets": ["ada", "agda", "alloy", "antlr", "applescript", "assembly", "augeas", "awk", "batchfile", "bison", "bluespec", "c", "c++", "c-sharp", "clojure", "cmake", "coffeescript", "common-lisp", "css", "cuda", "dart", "dockerfile", "elixir", "elm", "emacs-lisp","erlang", "f-sharp", "fortran", "glsl", "go", "groovy", "haskell","html", "idris", "isabelle", "java", "java-server-pages", "javascript", "julia", "kotlin", "lean", "literate-agda", "literate-coffeescript", "literate-haskell", "lua", "makefile", "maple", "markdown", "mathematica", "matlab", "ocaml", "pascal", "perl", "php", "powershell", "prolog", "protocol-buffer", "python", "r", "racket", "restructuredtext", "rmarkdown", "ruby", "rust", "sas", "scala", "scheme", "shell", "smalltalk", "solidity", "sparql", "sql", "stan", "standard-ml", "stata", "systemverilog", "tcl", "tcsh", "tex", "thrift", "typescript", "verilog", "vhdl", "visual-basic", "xslt", "yacc", "zig"],
-        "evaluation_splits": ["train"],
-    },
+#    "twitteraae": {
+#        "hf_repo": "lighteval/TwitterAAE",
+#        "hf_subsets": ["aa", "white"],
+#        "evaluation_splits": ["test"],
+#    },
+#    "culturax:eng": {
+#        "hf_repo": "yiyic/culturaX_eng",
+#        "hf_subsets": [],
+#        "evaluation_splits": ["test"],
+#    },
+#    "culturax:deu": {
+#        "hf_repo": "yiyic/culturaX_deu",
+#        "hf_subsets": [],
+#        "evaluation_splits": ["test"],
+#    },
+#    "culturax:esp": {
+#        "hf_repo": "yiyic/culturaX_esp",
+#        "hf_subsets": [],
+#        "evaluation_splits": ["test"],
+#    },
+#    "culturax:fra": {
+#        "hf_repo": "yiyic/culturaX_fra",
+#        "hf_subsets": [],
+#        "evaluation_splits": ["test"],
+#    },
+#    "culturax:ja": {
+#        "hf_repo": "yiyic/CulturaX_ja_test",
+#        "hf_subsets": [],
+#        "evaluation_splits": ["train"],
+#    },
+#    "culturax:yi": {
+#        "hf_repo": "yiyic/CulturaX_yi_test",
+#        "hf_subsets": [],
+#        "evaluation_splits": ["train"],
+#    },
+#    "culturax:he": {
+#        "hf_repo": "yiyic/CulturaX_he_test",
+#        "hf_subsets": [],
+#        "evaluation_splits": ["train"],
+#    },
+#    "culturax:am": {
+#        "hf_repo": "yiyic/CulturaX_am_test",
+#        "hf_subsets": [],
+#        "evaluation_splits": ["train"],
+#    },
+#    "culturax:mt": {
+#        "hf_repo": "yiyic/CulturaX_mt_test",
+#        "hf_subsets": [],
+#        "evaluation_splits": ["train"],
+#    },
+#    "culturax:hi": {
+#        "hf_repo": "yiyic/CulturaX_hi_test",
+#        "hf_subsets": [],
+#        "evaluation_splits": ["train"],
+#    },
+#    "culturax:ur": {
+#        "hf_repo": "yiyic/CulturaX_ur_test",
+#        "hf_subsets": [],
+#        "evaluation_splits": ["train"],
+#    },
+#    "culturax:gu": {
+#        "hf_repo": "yiyic/CulturaX_gu_test",
+#        "hf_subsets": [],
+#        "evaluation_splits": ["train"],
+#    },
+#    "culturax:si": {
+#        "hf_repo": "yiyic/CulturaX_si_test",
+#        "hf_subsets": [],
+#        "evaluation_splits": ["train"],
+#    },
+#    "culturax:pa": {
+#        "hf_repo": "yiyic/CulturaX_pa_test",
+#        "hf_subsets": [],
+#        "evaluation_splits": ["train"],
+#    },
+#    "culturax:tr": {
+#        "hf_repo": "yiyic/CulturaX_tr_test",
+#        "hf_subsets": [],
+#        "evaluation_splits": ["train"],
+#    },
+#    "culturax:kk": {
+#        "hf_repo": "yiyic/CulturaX_kk_test",
+#        "hf_subsets": [],
+#        "evaluation_splits": ["train"],
+#    },
+#    "culturax:ko": {
+#        "hf_repo": "yiyic/CulturaX_ko_test",
+#        "hf_subsets": [],
+#        "evaluation_splits": ["train"],
+#    },
+#    "culturax:mn": {
+#        "hf_repo": "yiyic/CulturaX_mn_test",
+#        "hf_subsets": [],
+#        "evaluation_splits": ["train"],
+#    },
+#    "culturax:mhr": {
+#        "hf_repo": "yiyic/CulturaX_mhr_test",
+#        "hf_subsets": [],
+#        "evaluation_splits": ["train"],
+#    },
+#    "culturax:fi": {
+#        "hf_repo": "yiyic/CulturaX_fi_test",
+#        "hf_subsets": [],
+#        "evaluation_splits": ["train"],
+#    },
+#    "culturax:ar": {
+#        "hf_repo": "yiyic/CulturaX_ar_test",
+#        "hf_subsets": [],
+#        "evaluation_splits": ["train"],
+#    },
+#    "culturax:hu": {
+#        "hf_repo": "yiyic/CulturaX_hu_test",
+#        "hf_subsets": [],
+#        "evaluation_splits": ["train"],
+#    },
+#    "culturax:zh": {
+#        "hf_repo": "yiyic/CulturaX_zh_test",
+#        "hf_subsets": [],
+#        "evaluation_splits": ["train"],
+#    },
+#    "the-stack": {
+#        "hf_repo": "bigcode/the-stack-smol-xs",
+#        "hf_subsets": ["ada", "agda", "alloy", "antlr", "applescript", "assembly", "augeas", "awk", "batchfile", "bison", "bluespec", "c", "c++", "c-sharp", "clojure", "cmake", "coffeescript", "common-lisp", "css", #"cuda", "dart", "dockerfile", "elixir", "elm", "emacs-lisp","erlang", "f-sharp", "fortran", "glsl", "go", #"groovy", "haskell","html", "idris", "isabelle", "java", "java-server-pages", "javascript", "julia", #"kotlin", "lean", "literate-agda", "literate-coffeescript", "literate-haskell", "lua", "makefile", "maple", #"markdown", "mathematica", "matlab", "ocaml", "pascal", "perl", "php", "powershell", "prolog", #"protocol-buffer", "python", "r", "racket", "restructuredtext", "rmarkdown", "ruby", "rust", "sas", "scala", #"scheme", "shell", "smalltalk", "solidity", "sparql", "sql", "stan", "standard-ml", "stata", #"systemverilog", "tcl", "tcsh", "tex", "thrift", "typescript", "verilog", "vhdl", "visual-basic", "xslt", #"yacc", "zig"],
+#        "evaluation_splits": ["train"],
+#    },
     "reddit-posts:popular-mix": {
         "hf_repo": "smwilliams/reddit_large_posts",
         "hf_subsets": [],
