588d21a8723df43d8947adeecab8d0a3988e1efe
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-04-28 21:39:32 -0700

    Bugfixes

--- katbench.py
+++ katbench.py
@@ -5,20 +5,15 @@ language_datasets = {
         "hf_subsets": ["arxiv", "bibliotik", "commoncrawl", "dm-mathematics", "enron", "europarl", "freelaw", "github", "gutenberg", "hackernews", "nih-exporter", "opensubtitles", "openwebtext2", "pubmed-abstracts", "pubmed-central", "stackexchange", "uspto", "wikipedia", "youtubesubtitles"],
         "evaluation_splits": ["test"],
     },
+    "pile:ubuntu-irc-broken": {
+        "hf_repo": "timaeus/pile-ubuntu_irc-broken",
+        "hf_subsets": [],
+        "evaluation_splits": ["train"],
+    },
     "pile:philpapers": {
         "hf_repo": "timaeus/pile-philpapers",
         "hf_subsets": [],
         "evaluation_splits": ["train"],
-    },
-    "irc_disentangle:ubuntu": {
-        "hf_repo": "jkkummerfeld/irc_disentangle",
-        "hf_subsets": ["ubuntu"],
-        "evaluation_splits": ["test"],
-    },
-    "irc_disentangle:channel_two": {
-        "hf_repo": "jkkummerfeld/irc_disentangle",
-        "hf_subsets": ["channel_two"],
-        "evaluation_splits": ["test"],
     },
     "twitteraae": {
         "hf_repo": "lighteval/TwitterAAE",
@@ -156,6 +151,8 @@ language_datasets = {
         "evaluation_splits": ["train"],
     },
 }
+
+# TODO: Include datasets that require post processing (jkkummerfeld/irc_disentangle, Fal7acy/4chan-archive, lemonilia/Elliquiy-Role-Playing-Forums_2023-04)
 
 import numpy as np
 from aenum import extend_enum
