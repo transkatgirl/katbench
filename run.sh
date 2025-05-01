3ad5bda39dd3bbc4d4ecb6fc9d3d884e438d098c
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-04-30 19:12:10 -0700

    Added TGI support, updated README

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 lighteval tasks list --custom-tasks katbench.py > /dev/null
-HF_HOME=cache lighteval accelerate \
-	"pretrained=$1,dtype=bfloat16,model_parallel=True" \
+HF_HOME=cache lighteval endpoint tgi \
+	model.yaml \
 	./tasks.txt \
 	--custom-tasks katbench.py \
-	--override-batch-size 1 \
 	--output-dir output
