1a43cf115e2e124b0527205abb946b14193fd68c
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-04-30 19:17:12 -0700

    Tweaked file handling

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
+if ! [ -e "model.yaml" ] ; then
+	echo "model:
+    inference_server_address: "http://your-inference-server.example.com"
+    inference_server_auth: null
+    model_name: null" > model.yaml
+fi
 lighteval tasks list --custom-tasks katbench.py > /dev/null
 HF_HOME=cache lighteval endpoint tgi \
 	model.yaml \
