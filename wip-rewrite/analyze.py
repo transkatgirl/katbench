ba0c3dbeca067484e42772ed4d40160b7011d593
Kat <10361870+transkatgirl@users.noreply.github.com>
2025-05-02 02:01:38 -0700

    Started work on analyze.py

--- wip-rewrite/analyze.py
+++ wip-rewrite/analyze.py
@@ -6,6 +6,16 @@ import time
 import datetime
 
 parser = argparse.ArgumentParser()
-parser.add_argument('bench_output', nargs="+", type=str)
+parser.add_argument('bench_data', nargs="+", type=str)
+parser.add_argument('--output_file', type=str)
 
 args = parser.parse_args()
+
+outputfilename = args.output_file or "metrics-" + str(round(time.time())) + ".json"
+
+for filename in args.bench_data:
+	file = open(filename)
+	for line in file:
+		linedata = json.loads(line)
+		print(linedata)
+	file.close()
